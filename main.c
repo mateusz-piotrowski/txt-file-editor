a8f25c403da9d5c8c7ae35e1e42d71b6d728e626
Mateusz Piotrowski <matt-pi@users.noreply.github.com>
2016-01-01 01:36:57 +0100

    Declare database structure in program code

--- main.c
+++ main.c
@@ -5,6 +5,16 @@
 #include <unistd.h>
 #include <stdio_ext.h>
 
+struct record
+{
+    int id_operator;
+    char name_operator;
+    int id_product;
+    char name_product;
+};
+
+struct record data;
+
 int main(void)
 {
     printf("Hello World\n");
