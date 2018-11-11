3f9535caa3d79ad15c89381ae77efd8535525557
Mateusz Piotrowski <mapispace@users.noreply.github.com>
2018-11-11 23:29:48 +0100

    Add "Hello World" template

---
+++ txt-file-editor/txt-file-editor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  txt-file-editor
+//
+//  Created by Mateusz Piotrowski on 11/11/18.
+//  Copyright © 2018 Mateusz Piotrowski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
